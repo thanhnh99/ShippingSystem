319620a9ff5b56e8b4b42fd850492eaf55325457
thanhnh <thanhnh99.amc@gmail.com>
2020-04-25 17:59:40 +0700

    check user commit

--- src/main/java/com/shippingsystem/repository/IUserRepositytory.java
+++ src/main/java/com/shippingsystem/repository/IUserRepositytory.java
@@ -1,7 +1,5 @@
 package com.shippingsystem.repository;
 
-import com.shippingsystem.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IUserRepositytory  extends JpaRepository<User,Long> {
+public interface IUserRepositytory  {
 }
